9c258d2ab9c7894562b6e5f33f43ff091c66345e
Aldo Acevedo <aldo.acevedo.toledo@gmail.com>
2022-02-19 14:06:48 +0100

    Implement basic conversion

--- Server/WordsEncoder.cs
+++ Server/WordsEncoder.cs
@@ -1,12 +1,131 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
 namespace Server
 {
     public static class WordsEncoder
     {
+        private static readonly Dictionary<int, string> Singulars = new()
+        {
+            [0] = "zero",
+            [1] = "one",
+            [2] = "two",
+            [3] = "three",
+            [4] = "four",
+            [5] = "five",
+            [6] = "six",
+            [7] = "seven",
+            [8] = "eight",
+            [9] = "nine",
+            [10] = "ten",
+            [11] = "eleven",
+            [12] = "twelve",
+            [13] = "thirteen",
+            [15] = "fifteen",
+            [18] = "eighteen",
+        };
+
+        private static readonly Dictionary<int, string> Tens = new()
+        {
+            [2] = "twenty",
+            [3] = "thirty",
+            [4] = "forty",
+            [5] = "fifty",
+            [6] = "sixty",
+            [7] = "seventy",
+            [8] = "eighty",
+            [9] = "ninety",
+        };
+
         public static string FromNumber(long number)
         {
-            // TODO: implement actual transformation
-            var transformed = $"Your number is: {number}";
-            return transformed;
+            var isSingular = Singulars.TryGetValue((int)number, out var converted);
+            if (isSingular)
+            {
+                return converted;
+            }
+
+            converted = "";
+
+            var str = number.ToString();
+            var numbers = str.ToArray().Select(c => Convert.ToInt32(c) - 48).ToArray();
+            int first;
+            int second;
+            int third;
+            int fourth;
+            int fifth;
+            int sixth;
+
+            switch (numbers.Length)
+            {
+                case 2:
+                    first = numbers[0];
+                    second = numbers[1];
+                    if (!Tens.TryGetValue(first, out var tens))
+                    {
+                        converted = Singulars[second] + "teen";
+                    }
+                    else
+                    {
+                        converted = second > 0
+                            ? tens + "-" + Singulars[second]
+                            : tens;
+                    }
+
+                    break;
+                case 3:
+                    first = numbers[0];
+                    converted = Singulars[first] + " hundred";
+                    if (number % 100 == 0) break;
+                    converted += " " + FromNumber(number % 100);
+                    break;
+                case 4:
+                    first = numbers[0];
+                    converted = Singulars[first] + " thousand";
+                    if (number % 1000 == 0) break;
+                    converted += " " + FromNumber(number % 1000);
+                    break;
+                case 5:
+                    var two = str[..2];
+                    var portion2 = Convert.ToInt64(two);
+                    converted = FromNumber(portion2) + " thousand";
+                    if (number % 1000 == 0) break;
+                    converted += " " + FromNumber(number % 1000);
+                    break;
+                case 6:
+                    var three = str[..3];
+                    var portion3 = Convert.ToInt64(three);
+                    converted = FromNumber(portion3) + " thousand";
+                    if (number % 1000 == 0) break;
+                    converted += " " + FromNumber(number % 1000);
+                    break;
+                case 7:
+                    first = numbers[0];
+                    converted = Singulars[first] + " million";
+                    if (number % Math.Pow(10, 6) == 0) break;
+                    var rest1 = Convert.ToInt64(str[1..]);
+                    converted += " " + FromNumber(rest1);
+                    break;
+                case 8:
+                    first = Convert.ToInt32(str[..2]);
+                    converted = FromNumber(first) + " million";
+                    if (number % Math.Pow(10, 6) == 0) break;
+                    var rest2 = Convert.ToInt64(str[2..]);
+                    converted += " " + FromNumber(rest2);
+                    break;
+                case 9:
+                    first = Convert.ToInt32(str[..3]);
+                    converted = FromNumber(first) + " million";
+                    if (number % Math.Pow(10, 6) == 0) break;
+                    var rest3 = Convert.ToInt64(str[3..]);
+                    converted += " " + FromNumber(rest3);
+                    break;
+            }
+
+
+            return converted;
         }
     }
 }

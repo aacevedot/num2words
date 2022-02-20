db451f8df6c6ec2382c90d0f8614208ba5ae0e8d
Aldo Acevedo <aldo.acevedo.toledo@gmail.com>
2022-02-20 19:40:21 +0100

    Add very basic client implementations

---
+++ CliClient/Program.cs
@@ -0,0 +1,60 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+using num2words;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace CliClient
+{
+    class Program
+    {
+        private static readonly Regex Cleaner = new Regex(@"\s+");
+        
+        public static async Task Main(string[] args)
+        {
+            var clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (_, _, _, _) => true;
+            var httpClient = new HttpClient(clientHandler);
+
+            const string server = "https://localhost:5001";
+            var channel = GrpcChannel.ForAddress(server, new GrpcChannelOptions
+            {
+                HttpClient = httpClient
+            });
+            var client = new Parser.ParserClient(channel);
+
+            while (true)
+            {
+                Console.Write("Input a number [enter: Exit]: ");
+                var input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) break;
+
+                double number;
+                try
+                {
+                    input = Cleaner.Replace(input, "");
+                    number = Convert.ToDouble(input);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(input);
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Invalid input, try again!");
+                    continue;
+                }
+
+                try
+                {
+                    var request = new NumberRequest { Number = number };
+                    var response = await client.FromNumberToWordsAsync(request);
+                    Console.WriteLine(response.Words);
+                }
+                catch
+                {
+                    Console.WriteLine("error while communicating with the server, try again!");
+                }
+            }
+        }
+    }
+}
---
+++ WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Grpc.Net.Client;
+using num2words;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Parser.ParserClient? _client;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Connect();
+        }
+
+        private void Connect()
+        {
+            const string server = "https://localhost:5001";
+            TextHolder.Text = $"Connecting to: {server}";
+
+            try
+            {
+                var clientHandler = new HttpClientHandler();
+                clientHandler.UseProxy = false;
+                clientHandler.ServerCertificateCustomValidationCallback =
+                    (sender, cert, chain, sslPolicyErrors) => true;
+                var httpClient = new HttpClient(clientHandler);
+
+                var channel = GrpcChannel.ForAddress(server, new GrpcChannelOptions
+                {
+                    HttpClient = httpClient
+                });
+                _client = new Parser.ParserClient(channel);
+                TextHolder.Text = $"Connected to: {server}";
+            }
+            catch (Exception e)
+            {
+                TextHolder.Text = e.Message;
+            }
+        }
+
+        private async void convertButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_client == null)
+            {
+                TextHolder.Text = "Not connected!";
+                return;
+            }
+
+            var number = Convert.ToDouble(InputField.Text);
+
+            var req = new NumberRequest
+            {
+                Number = number
+            };
+
+            string message;
+            try
+            {
+                var res = await _client.FromNumberToWordsAsync(req);
+                message = res.Words;
+            }
+            catch (Exception error)
+            {
+                message = error.Message;
+            }
+
+            TextHolder.Text = message;
+        }
+    }
+}

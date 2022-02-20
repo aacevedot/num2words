abc819e46987cd8b7aa8b46f889588f4f1088acb
Aldo Acevedo <aldo.acevedo.toledo@gmail.com>
2022-02-20 22:40:40 +0100

    Tiny refactoring. Add catch blocks for RPC exceptions.

--- WpfClient/MainWindow.xaml.cs
+++ WpfClient/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Grpc.Core;
 
 namespace WpfClient
 {
@@ -33,34 +35,37 @@ namespace WpfClient
 
         private void Connect()
         {
+            // TODO: Check if the address should be provided by the user
             const string server = "https://localhost:5001";
-            TextHolder.Text = $"Connecting to: {server}";
+
+            var httpClientHandler = new HttpClientHandler();
+            // NOTE: Only for dev purposes. Prod applications should use valid certs.
+            httpClientHandler.ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
 
             try
             {
-                var clientHandler = new HttpClientHandler();
-                clientHandler.UseProxy = false;
-                clientHandler.ServerCertificateCustomValidationCallback = (_, _, _, _) => true;
-                var httpClient = new HttpClient(clientHandler);
-
                 var channel = GrpcChannel.ForAddress(server, new GrpcChannelOptions
                 {
-                    HttpClient = httpClient
+                    HttpHandler = httpClientHandler
                 });
                 _client = new Parser.ParserClient(channel);
-                TextHolder.Text = $"Connected to: {server}";
+                TextHolder.Text = "Write a number below...";
             }
             catch (Exception e)
             {
+                // TODO: This should not be visible!
                 TextHolder.Text = e.Message;
             }
         }
 
-        private async void convertButton_Click(object sender, RoutedEventArgs e)
+        private async void ConvertButton_Click(object sender, RoutedEventArgs e)
         {
             if (_client == null)
             {
-                TextHolder.Text = "Not connected!";
+                // TODO: Check if this makes sense! (Would the client ever be null???)
+                // TODO: If yes, then trying to reconnect here might be good idea
+                TextHolder.Text = "Connection failed!";
                 return;
             }
 
@@ -77,9 +82,17 @@ namespace WpfClient
                 var res = await _client.FromNumberToWordsAsync(req);
                 message = res.Words;
             }
-            catch (Exception error)
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.Internal)
             {
-                message = error.Message;
+                message = "An internal server error was detected. Please, try again!";
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.Unavailable)
+            {
+                message = "The server is unavailable. Please, try again later!";
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.DeadlineExceeded)
+            {
+                message = "It took too long, the server is not responding. Please, try again!";
             }
 
             TextHolder.Text = message;

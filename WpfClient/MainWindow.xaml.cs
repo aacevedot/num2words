41d1c8780a927fdbabeea62807e76a9259c492f3
Aldo Acevedo <aldo.acevedo.toledo@gmail.com>
2022-02-21 20:31:59 +0100

    Remove unused directives

--- WpfClient/MainWindow.xaml.cs
+++ WpfClient/MainWindow.xaml.cs
@@ -1,21 +1,9 @@
 ﻿using Grpc.Net.Client;
 using num2words;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Globalization;
 using System.Net.Http;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Grpc.Core;
 using System.Text.RegularExpressions;
 
@@ -95,7 +83,7 @@ namespace WpfClient
             }
 
             PrimaryText.Text = "Sending request...";
-            SecondaryText.Text = DateTime.Now.ToString();
+            SecondaryText.Text = DateTime.Now.ToString(CultureInfo.InvariantCulture);
 
             var req = new NumberRequest
             {
@@ -124,7 +112,7 @@ namespace WpfClient
             }
 
             PrimaryText.Text = message;
-            SecondaryText.Text = DateTime.Now.ToString();
+            SecondaryText.Text = DateTime.Now.ToString(CultureInfo.InvariantCulture);
         }
     }
 }

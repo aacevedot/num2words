536c80cf782dfc3d7d263740e35defbf03972929
Aldo Acevedo <aldo.acevedo.toledo@gmail.com>
2022-02-23 00:00:10 +0100

    WpfClient: Refactoring

--- Server/ParserService.cs
+++ Server/ParserService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
@@ -16,11 +17,33 @@ namespace Server
 
         public override Task<WordsResponse> FromNumberToWords(NumberRequest request, ServerCallContext context)
         {
-            // TODO: Capture exceptions
-            var encoded = WordsEncoder.DoubleToCurrency(request.Number);
+            string converted;
+            try
+            {
+                converted = WordsEncoder.DoubleToCurrency(request.Number);
+            }
+            catch (FormatException ex)
+            {
+                _logger.LogError(ex.Message);
+                var status = new Status(StatusCode.FailedPrecondition, "number cannot be converted");
+                throw new RpcException(status);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                _logger.LogError(ex.Message);
+                var status = new Status(StatusCode.InvalidArgument, ex.Message);
+                throw new RpcException(status);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical(ex.Message);
+                var status = new Status(StatusCode.Internal, "critical error while processing the request");
+                throw new RpcException(status);
+            }
+
             var response = new WordsResponse
             {
-                Words = encoded
+                Words = converted
             };
 
             return Task.FromResult(response);

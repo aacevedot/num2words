0b0ac4e7ab84a88f78bbc87f51c194a6c88f3d30
Aldo Acevedo <aldo.acevedo.toledo@gmail.com>
2022-02-22 22:27:15 +0100

    Server: Split conversion logic

--- Server.Test/Converters/CurrencyConverterTests.cs
+++ Server.Test/Converters/CurrencyConverterTests.cs
@@ -22,72 +22,9 @@ namespace Server.Test.Converters
         [InlineData(45100, "forty-five thousand one hundred dollars")]
         [InlineData(999999999.99,
             "nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred ninety-nine dollars and ninety-nine cents")]
-        public void Test_DoubleToCurrency(double input, string expected)
+        public void Test_FromDoubleToCurrency(double input, string expected)
         {
             var words = CurrencyConverter.FromDoubleToCurrency(input);
-            Assert.Equal(expected, words);
-        }
-
-        [Theory]
-        [InlineData(12.12, new[] { "twelve", "twelve" })]
-        public void Test_DoubleToWords(double input, string[] expected)
-        {
-            var words = CurrencyConverter.DoubleToWords(input);
-            Assert.Equal(expected, words);
-        }
-
-        [Theory]
-        [InlineData(0, "zero")]
-        [InlineData(1, "one")]
-        [InlineData(10, "ten")]
-        [InlineData(11, "eleven")]
-        [InlineData(12, "twelve")]
-        [InlineData(19, "nineteen")]
-        [InlineData(17, "seventeen")]
-        [InlineData(16, "sixteen")]
-        [InlineData(25, "twenty-five")]
-        [InlineData(42, "forty-two")]
-        [InlineData(80, "eighty")]
-        [InlineData(50, "fifty")]
-        [InlineData(109, "one hundred nine")]
-        [InlineData(118, "one hundred eighteen")]
-        [InlineData(100, "one hundred")]
-        [InlineData(120, "one hundred twenty")]
-        [InlineData(152, "one hundred fifty-two")]
-        [InlineData(1259, "one thousand two hundred fifty-nine")]
-        [InlineData(45100, "forty-five thousand one hundred")]
-        [InlineData(100000, "one hundred thousand")]
-        [InlineData(112000, "one hundred twelve thousand")]
-        [InlineData(711010, "seven hundred eleven thousand ten")]
-        [InlineData(356137, "three hundred fifty-six thousand one hundred thirty-seven")]
-        [InlineData(1000000, "one million")]
-        [InlineData(1010012, "one million ten thousand twelve")]
-        [InlineData(1234987, "one million two hundred thirty-four thousand nine hundred eighty-seven")]
-        [InlineData(12345678, "twelve million three hundred forty-five thousand six hundred seventy-eight")]
-        [InlineData(13089050, "thirteen million eighty-nine thousand fifty")]
-        [InlineData(123456789,
-            "one hundred twenty-three million four hundred fifty-six thousand seven hundred eighty-nine")]
-        [InlineData(999999999,
-            "nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred ninety-nine")]
-        [InlineData(9999999999,
-            "nine billion nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred ninety-nine")]
-        [InlineData(99999999999,
-            "ninety-nine billion nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred ninety-nine")]
-        [InlineData(999999999999,
-            "nine hundred ninety-nine billion nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred ninety-nine")]
-        [InlineData(9999999999999, null)]
-        [InlineData(99999999999999, null)]
-        [InlineData(999999999999999, null)]
-        [InlineData(9223372036854775807, null)]
-        public void Test_IntegerToWords(long input, string expected)
-        {
-            if (string.IsNullOrEmpty(expected))
-            {
-                Assert.Throws<ArgumentOutOfRangeException>(() => CurrencyConverter.IntegerToWords(input));
-                return;
-            }
-
-            var words = CurrencyConverter.IntegerToWords(input);
             Assert.Equal(expected, words);
         }
     }
--- Server/Converters/CurrencyConverter.cs
+++ Server/Converters/CurrencyConverter.cs
@@ -6,163 +6,60 @@ namespace Server.Converters
 {
     public static class CurrencyConverter
     {
-        private static readonly Dictionary<int, string> SingularNumbers = new()
+        private static readonly Currency Currency = new()
         {
-            [0] = "zero",
-            [1] = "one",
-            [2] = "two",
-            [3] = "three",
-            [4] = "four",
-            [5] = "five",
-            [6] = "six",
-            [7] = "seven",
-            [8] = "eight",
-            [9] = "nine",
-            [10] = "ten",
-            [11] = "eleven",
-            [12] = "twelve",
-            [13] = "thirteen",
-            [15] = "fifteen",
-            [18] = "eighteen"
-        };
-
-        private static readonly Dictionary<int, string> TyNumbers = new()
-        {
-            [2] = "twenty",
-            [3] = "thirty",
-            [4] = "forty",
-            [5] = "fifty",
-            [6] = "sixty",
-            [7] = "seventy",
-            [8] = "eighty",
-            [9] = "ninety"
+            AmountSingular = "dollar",
+            AmountPlural = "dollars",
+            CoinsSingular = "cent",
+            CoinsPlural = "cents",
+            Symbol = "$"
         };
 
         public static string FromDoubleToCurrency(double number)
         {
-            var converted = DoubleToWords(number);
+            // TODO: Check if this rounding approach makes sense
+            number = Math.Round(number, 2, MidpointRounding.ToZero);
 
-            var dollars = converted[0];
-            var suffix = dollars == "one" ? "dollar" : "dollars";
-            var asCurrency = dollars + " " + suffix;
+            var segments = SplitNumberInSegments(number);
+            var totalSegments = segments.Length;
 
-            if (converted.Length < 2)
+            // amount segment
+            var segment = segments[0];
+            var amount = Convert.ToInt64(segment);
+            var amountAsWords = IntegerConverter.IntegerToWords(amount);
+
+            var suffix = amountAsWords.Equals("one") ? Currency.AmountSingular : Currency.AmountPlural;
+            var asCurrency = amountAsWords + " " + suffix;
+
+            if (totalSegments < 2)
             {
                 return asCurrency;
             }
 
-            var cents = converted[1];
-            suffix = cents == "one" ? "cent" : "cents";
-            asCurrency += " and " + cents + " " + suffix;
+            // coins segment
+            segment = segments[1];
+            var coins = Convert.ToInt64(segment);
+            coins = FixTens(segment, coins);
+            var coinsAsWords = IntegerConverter.IntegerToWords(coins);
+
+            suffix = coinsAsWords.Equals("one") ? Currency.CoinsSingular : Currency.CoinsPlural;
+            asCurrency += " and " + coinsAsWords + " " + suffix;
 
             return asCurrency;
         }
 
-        public static string[] DoubleToWords(double number)
+        private static string[] SplitNumberInSegments(double number)
         {
-            // TODO: Check if this rounding approach makes sense
-            number = Math.Round(number, 2, MidpointRounding.ToZero);
-
             var str = number.ToString(CultureInfo.InvariantCulture);
-            var segments = str.Split('.');
-            var totalSegments = segments.Length;
-
-            // if a "micro-optimization" is required; then, refactor this
-            // for using an array of primitives, e.g., new string[len]
-            var converted = new List<string>();
-            var words = "";
-
-            var segment = segments[0];
-            var asInteger = Convert.ToInt64(segment);
-
-            // sanity check for negative numbers
-            if (asInteger < 0)
-            {
-                words += "minus ";
-                asInteger = Math.Abs(asInteger);
-            }
-
-            words += IntegerToWords(asInteger);
-
-            converted.Add(words);
-
-            if (totalSegments < 2)
-            {
-                return converted.ToArray();
-            }
-
-            segment = segments[1];
-            asInteger = Convert.ToInt64(segment);
-
-            // sanity check for numbers lower than 10 after separator
-            if (!segment.StartsWith("0") && asInteger < 10)
-            {
-                asInteger *= 10;
-            }
-
-            words = IntegerToWords(asInteger);
-            converted.Add(words);
-
-            return converted.ToArray();
+            var separator = CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator;
+            var segments = str.Split(separator);
+            return segments;
         }
 
-        public static string IntegerToWords(long number)
+        private static long FixTens(string segment, long number)
         {
-            if (SingularNumbers.TryGetValue((int)number, out var asWords))
-            {
-                return asWords;
-            }
-
-            var str = number.ToString();
-            var digits = str.Length;
-            int offset;
-            string suffix;
-
-            switch (digits)
-            {
-                case 2:
-                    offset = digits - 1;
-                    suffix = "tens";
-                    break;
-                case 3:
-                    offset = digits - 2;
-                    suffix = "hundred";
-                    break;
-                case >= 4 and <= 6:
-                    offset = digits - 3;
-                    suffix = "thousand";
-                    break;
-                case >= 7 and <= 9:
-                    offset = digits - 6;
-                    suffix = "million";
-                    break;
-                case >= 10 and <= 12:
-                    offset = digits - 9;
-                    suffix = "billion";
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(number), "the number is too long");
-            }
-
-            var head = Convert.ToInt32(str[..offset]);
-            var tail = Convert.ToInt32(str[offset..]);
-            if (suffix.Equals("tens"))
-            {
-                if (TyNumbers.TryGetValue(head, out var ty))
-                {
-                    asWords = tail > 0 ? ty + "-" + SingularNumbers[tail] : ty;
-                    return asWords;
-                }
-
-                asWords = SingularNumbers[tail] + "teen";
-                return asWords;
-            }
-
-            asWords = IntegerToWords(head) + " " + suffix;
-            if (tail == 0) return asWords;
-
-            asWords += " " + IntegerToWords(tail);
-            return asWords;
+            // sanity check for numbers lower than 10 after separator
+            return !segment.StartsWith("0") && number < 10 ? number *= 10 : number;
         }
     }
 }
